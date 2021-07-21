9ba9fcd59f66ff99391c158391d353194d54fb96
maxgertzen <maxgertzen@gmail.com>
2021-07-21 20:31:17 +0300

    add utc time format for db

--- models/user.model.js
+++ models/user.model.js
@@ -6,6 +6,8 @@ const User = function (user) {
     this.first_name = user.firstName;
     this.last_name = user.lastName;
     this.accept_mail = user.acceptMail || 0;
+    this.birth_date = user.birth_date;
+    this.bio = user.bio
 }
 
 
@@ -72,9 +74,9 @@ User.findByEmail = (userCreds, result) => {
             result(err, null);
             return;
         }
-
+        console.log(userCreds)
         if (res.length) {
-            console.log(`found user: ${res[0]} `);
+            console.log(`found user: ${res[0].firstName} `);
             if (res[0].password === userCreds.password) {
                 console.log(res)
                 result(null, res[0]);
